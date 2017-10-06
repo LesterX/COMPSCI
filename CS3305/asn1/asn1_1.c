7138c863d832775cb33ed5c0aa1e5b096956f3f0
LesterX <xymtz@live.com>
2017-10-06 15:24:50 -0400

    Update: CS 3305 Assignment 1 completed

--- CS3305/asn1/asn1_1.c
+++ CS3305/asn1/asn1_1.c
@@ -1,3 +1,9 @@
+/*
+Computer Science 3305
+Assignment 1
+Yimin Xu
+250876566
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -16,8 +22,11 @@ int main()
         exit(pid);
     }else if (pid > 0)
     {
+        //Parent
+        printf("parent process (PID %d) created child_1 (PID %d)\n",getpid(),pid);
+        printf("parent (PID %d) is waiting for child_1 (PID %d) to complete before creating child_2\n",getpid(),pid);
         wait(NULL);
-	pid = fork();
+        pid = fork();
     	if (pid < 0)
     	{
         	perror("fork");
@@ -25,15 +34,19 @@ int main()
         	exit(pid);
     	}else if (pid == 0)
     	{
-        	printf("parent (PID %d) created child_2 (PID %d)\n",getppid(),getpid());
+            //Child 2
         	printf("child_2 (PID %d) is calling an external program B.out and leaving child_2...\n",getpid());
         	printf("From the external program B: \n");
         	execl("./B.out","./B.out",NULL);
+    	}else
+    	{
+            //Parent
+            printf("parent (PID %d) created child_2 (PID %d)\n",getpid(),pid);
+
     	}
     }else
     {
-        printf("parent process (PID %d) created child_1 (PID %d)\n",getppid(),getpid());
-        printf("parent (PID %d) is waiting for child_1 (PID %d) to complete before creating child_2\n",getppid(),getpid());
+        //Child 1
         pid = fork();
         if (pid < 0)
         {
@@ -41,10 +54,10 @@ int main()
             printf("Error\n");
             exit(pid);
         }
-        else if (pid == 0)
+        else if (pid > 0)
         {
-            printf("child_1 (PID %d) created child_1.1 (PID %d)\n",getppid(),getpid());
-            printf("child_1 (PID %d) is now completed\n",getpid());
+            printf("child_1 (PID %d) created child_1.1 (PID %d)\n",getpid(),pid);
+            printf("child_1 (PID %d) is now completed\n",pid);
         }
     }
 }
--- CS3305/asn1/asn1_2.c
+++ CS3305/asn1/asn1_2.c
@@ -1,3 +1,8 @@
+/* Computer Science 3305
+ * Assignment 1
+ * Yimin Xu
+ * 250876566 */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -5,6 +10,8 @@
 #include <unistd.h>
 #include <errno.h>
 #include <wait.h>
+
+/* Comment is actually shown in the output...so...you know... */
 
 int main()
 {
@@ -24,7 +31,6 @@ int main()
 	else if (pid == 0)
 	{
 		//Child
-        printf("parent (PID %d) created a child (PID %d)\n",getppid(),getpid());
         close(fd[0]);
 		printf("child (PID %d) Reading Y = ",getpid());
         scanf(" %s", in_Y);
@@ -34,15 +40,16 @@ int main()
 	}else
 	{
 		//Parent
+        printf("parent (PID %d) created a child (PID %d)\n",getpid(),pid);
+        wait(NULL);
         close(fd[1]);
 		printf("parent (PID %d) Reading X = ",getpid());
         scanf(" %s", in_X);
         printf(" from the user\n");
 		X = atoi(in_X);
-        wait(NULL);
         read(fd[0],in_Y,100);
         printf("parent (PID %d) Reading Y from the pipe (Y = %s)\n",getpid(),in_Y);
         Y = atoi(in_Y);
-		printf("parent (PID %d) adding X + Y = %d",getpid(),X + Y);
+		printf("parent (PID %d) adding X + Y = %d\n",getpid(),X + Y);
 	}
 }
