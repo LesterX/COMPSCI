5cbb66f710bf2eea22b96d034fdf792a1621d553
LesterX <xymtz@live.com>
2017-12-07 19:36:40 -0500

    CS3305 Assignment 3 Finished

--- CS3305/asn3/asn3.c
+++ CS3305/asn3/asn3.c
@@ -6,18 +6,6 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-/*
-#define thread_1_req 400
-#define thread_2_req 300
-#define thread_3_req 200
-#define thread_4_req 150
-
-void  *withdraw_money_thread_1();
-void  *withdraw_money_thread_2();
-void  *withdraw_money_thread_3();
-void  *withdraw_money_thread_4();
-*/
-
 typedef struct account_request
 {
 	char request_type;
@@ -28,26 +16,72 @@ typedef struct account_request
 
 pthread_mutex_t lock;
 
-int shared_balance = 0;
+int balance_acc1 = 0;
+int balance_acc2 = 0;
+int balance_acc3 = 0;
+int balance_acc4 = 0;
+int balance_acc5 = 0;
 
-void *withdraw_money_thread(int amount)
+int *find_account(int account)
 {
+	if (account == 1)
+		return &balance_acc1;
+	else if (account == 2)
+		return &balance_acc2;
+	else if (account == 3)
+		return &balance_acc3;
+	else if (account == 4)
+		return &balance_acc4;
+	else if (account == 5)
+		return &balance_acc5;
+}
+
+void *bank_account_request(void* rqst)
+{
+	request *rq = (request*) rqst;
 
 	pthread_mutex_lock(&lock);
 
-	printf("\n Processing withdraw request.. current balance %d", shared_balance);
+	//printf("\n\nProcessing request... \n");
+	//printf("Current balance of Account 1: %d\nCurrent balance of Account 2: %d\n", balance_acc1, balance_acc2);
 
-
-	if (shared_balance >= amount)
-		printf("\n Withdraw amount of %d is Authorized", amount);
-	else
+	if (rq->request_type == 'd')
 	{
-		printf("\n Request denied");
-		return 0;
+		//printf("Deposit to %d: $%d\n", rq->account, rq->amount);
+		*find_account(rq->account) += rq->amount;
+	}else if (rq->request_type == 'w')
+	{
+		//printf("Withdraw from %d: $%d\n", rq->account, rq->amount);
+		if (*find_account(rq->account) < rq->amount)
+		{
+			//printf("Request denied: balance is not enough\n");
+			pthread_mutex_unlock(&lock);
+			return 0;
+		}
+		else
+		{
+			*find_account(rq->account) -= rq->amount;
+			//printf("Withdraw Authorized\n");
+		}
+	}else if (rq->request_type == 't')
+	{
+		//printf("Transfer from %d to %d: $%d\n", rq->account, rq->target, rq->amount);
+		if (*find_account(rq->account) < rq->amount)
+		{
+			//printf("Request denied: balance is not enough\n");
+			pthread_mutex_unlock(&lock);
+			return 0;
+		}
+		else
+		{
+			*find_account(rq->account) -= rq->amount;
+			*find_account(rq->target) += rq->amount;
+			//printf("Transfer Authorized\n");
+		}
 	}
 
-	shared_balance = shared_balance - amount;
-	printf("\n Updated balance %d", shared_balance);
+	//printf("Updated Balance:\n");
+	//printf("Account 1: %d\nAccount 2: %d\n", balance_acc1, balance_acc2);
 
 	pthread_mutex_unlock(&lock);
 
@@ -58,7 +92,8 @@ int main(void)
 {
 	int k = 2,n = 2,m = 10;
 
-	request rqs[m + n];
+	request d_rq[n * 4];
+	request c_rq[m * 4];
 
 	//Read file
     FILE *fp;
@@ -71,88 +106,119 @@ int main(void)
         return 0;
     }
 
-    int count = 0;
+    int d_count = 0, c_count = 0;
 
     while(fgets(line,sizeof(line),fp))
     {
-    	printf("Line: %s\n", line);
+
     	char *str = strtok(line," ");
+    	char user = str[0];
+
     	str = strtok(NULL," ");
     	
     	int str_count = 0;
     	while (str != NULL && str[0] != ' ')
     	{	
-    		printf("	Type: %s\n",str);
-    		rqs[count].request_type = str[0];
-    		str = strtok(NULL," ");
-    		rqs[count].account = str[3];
-    		printf("	Account: %s\n",str);
-
-    		if (rqs[count].request_type == 't')
+    		if (user == 'd')
     		{
-    			str = strtok(NULL," ");
-    			printf("	Target: %s\n",str);
-    			rqs[count].target = atoi(str);
+    			d_rq[d_count].request_type = str[0];
+
+	    		str = strtok(NULL," ");
+	    		
+	    		d_rq[d_count].account = str[3] - '0';
+
+	    		if ((d_rq)[d_count].request_type == 't')
+	    		{
+	    			str = strtok(NULL," ");
+	    			d_rq[d_count].target = str[3] - '0';
+	    		}else
+	    			d_rq[d_count].target = d_rq[d_count].account;
+	    		
+	    		str = strtok(NULL," ");
+
+	    		d_rq[d_count].amount = atoi(str);
+
+	    		str = strtok(NULL," ");
+	    		d_count ++;
+    		}else
+    		{
+    			c_rq[c_count].request_type = str[0];
+
+	    		str = strtok(NULL," ");
+	    		
+	    		c_rq[c_count].account = str[3] - '0';
+	    	
+	    		if ((c_rq)[c_count].request_type == 't')
+	    		{
+	    			str = strtok(NULL," ");
+	    			c_rq[c_count].target = str[3] - '0';
+	    		}else
+	    		{
+					c_rq[c_count].target = c_rq[c_count].account;
+					//printf("Client Count = %d!!!!!!!!\n\n\n",c_count);
+			    }
+	    		
+
+	    		str = strtok(NULL," ");
+
+	    		c_rq[c_count].amount = atoi(str);
+
+	    		str = strtok(NULL," ");
+	    			
+	    		c_count ++;
     		}
-    		
-    		str = strtok(NULL," ");
-    		printf("	Amount: %s\n\n",str);
-
-    		if (str == NULL)
-    			printf("Hi\n");
-
-    		rqs[count].amount = atoi(str);
-
-    		str = strtok(NULL," ");
-    		count ++;
-
     	}
     }
-	
-	/*
-	int i;
-	int err_thread;
 
-	pthread_t thread_1, thread_2, thread_3, thread_4;
+    fclose(fp);
 
+    int err_thread;
+    
+    pthread_t* ptr_d = malloc(sizeof(pthread_t) * d_count);
+    pthread_t* ptr_c = malloc(sizeof(pthread_t) * c_count);
 
-	if (pthread_mutex_init(&lock, NULL) != 0)
-	    {
+    if (pthread_mutex_init(&lock, NULL) != 0)
+	{
 	        printf("\n mutex init failed\n");
 	        return 1;
-	    }
+	}
 
+    for (int i = 0; i < d_count; i ++)
+    {
+		err_thread = pthread_create(&ptr_d[i], NULL, bank_account_request, (void *) &d_rq[i]);
+		if (err_thread != 0)
+			printf("Error in creating thread\n");
+	}
 
-	err_thread = pthread_create(&thread_1, NULL, &withdraw_money_thread_1, NULL); 
+    for (int i = 0; i < d_count; i ++)
+    	pthread_join(ptr_d[i], NULL);
 
-	if (err_thread != 0)
-		printf("\n Error creating thread_1");
+    for (int i = 0; i < c_count; i ++)
+    {
+    	pthread_create(&ptr_c[i], NULL, bank_account_request, (void *) &c_rq[i]);
+    	if (err_thread != 0)
+			printf("Error in creating thread\n");
+	}
 
+    for (int i = 0; i < c_count; i ++)
+    	pthread_join(ptr_c[i], NULL);
 
+    pthread_mutex_destroy(&lock);
 
-	err_thread = pthread_create(&thread_2, NULL, &withdraw_money_thread_2, NULL); 
+    fp = fopen("Assignment_3_output_file.txt","w");
+    if (fp == NULL)
+    {
+    	printf("Error opening file\n");
+    	exit(1);
+    }
 
-	if (err_thread != 0)
-		printf("\n Error creating thread_2");
-
-	err_thread = pthread_create(&thread_3, NULL, &withdraw_money_thread_3, NULL); 
-
-	if (err_thread != 0)
-		printf("\n Error creating thread_3");
-
-	err_thread = pthread_create(&thread_4, NULL, &withdraw_money_thread_4, NULL); 
-
-	if (err_thread != 0)
-		printf("\n Error creating thread_4");
-
-	pthread_join(thread_1, NULL);
-	pthread_join(thread_2, NULL);
-	pthread_join(thread_3, NULL);
-	pthread_join(thread_4, NULL);
-
-
-	pthread_mutex_destroy(&lock);
+    for (int i = 1; i < 5; i ++)
+    {
+    	if (*find_account(i) > 0)
+    		fprintf(fp,"Account %d: $%d    ", i, *find_account(i));
+    }
+    
+    fclose(fp);
 
 	return 0;
-	*/
 }
